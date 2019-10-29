e8c682de65d7923fecd6a5c9a020c1f88d5ba02b
webwerks <webwerks@webwerkss-Mac-mini.local>
2019-10-29 12:21:52 +0530

    finally run

--- android/app/src/main/java/com/counterappredux/MainApplication.java
+++ android/app/src/main/java/com/counterappredux/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.henninghall.date_picker.DatePickerPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
--- src/RandomDate.js
+++ src/RandomDate.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, TouchableOpacity, Text, Alert} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Alert,
+  Image,
+} from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import {Button} from '../component/Button';
 import {connect} from 'react-redux';
@@ -19,7 +26,6 @@ class RandomDate extends Component {
   render() {
     const {viewStyle, buttonStyle, buttonTextStyle} = Container;
     const {navigate} = this.props.navigation;
-
     return (
       <View
         style={{
@@ -28,9 +34,33 @@ class RandomDate extends Component {
           flex: 1,
           backgroundColor: this.props.theme,
         }}>
+        <Image
+          style={{height: 250, width: 180, borderRadius: 5}}
+          source={require('../asset/image/TimeMatchine.jpg')}
+        />
+        <Text
+          style={{
+            color: 'white',
+            marginTop: 20,
+            marginLeft: 10,
+            marginRight: 10,
+            fontSize: 15,
+          }}>
+          The Time Machine is a science fiction novella by H. G. Wells,
+          published in 1895 and written as a frame narrative. The work is
+          generally credited with the popularization of the concept of time
+          travel by using a vehicle or device to travel purposely and
+          selectively forward or backward through time.
+        </Text>
         <DatePicker
           mode="date"
-          style={{backgroundColor: '#ff69b4', width: 160, borderRadius: 5}}
+          style={{
+            backgroundColor: '#ff69b4',
+            width: 180,
+            height: 51,
+            borderRadius: 5,
+            marginTop: 20,
+          }}
           date={this.state.date}
           onDateChange={date => {
             var myDate = Math.floor(new Date(date).valueOf() / 1000);
@@ -45,16 +75,61 @@ class RandomDate extends Component {
           }}
           placeholder="Select Date"
           confirmBtnText="Done"
-          cancelBtnText="cancel"
+          cancelBtnText="Cancel"
           customStyles={{
             dateIcon: {
               position: 'absolute',
-              left: 0,
+              left: 10,
               right: 0,
-              marginRight: 0,
+              marginRight: 10,
+              borderRadius: 5,
+              backgroundColor: '#00bfff',
             },
             dateInput: {
-              marginLeft: 0,
+              backgroundColor: '#dc143c',
+              borderColor: '#00ff00',
+              borderRadius: 5,
+              margin: 5,
+            },
+            btnTextConfirm: {
+              color: '#ff00ff',
+              // backgroundColor: 'black',
+              marginTop: 10,
+              height: 30,
+              width: 80,
+              textAlign: 'center',
+              borderRadius: 5,
+              fontSize: 20,
+            },
+            btnTextCancel: {
+              color: '#191970',
+              // backgroundColor: 'black',
+              marginTop: 10,
+              height: 30,
+              width: 80,
+              textAlign: 'center',
+              borderRadius: 10,
+              fontSize: 20,
+            },
+            datePickerCon: {
+              // backgroundColor: '#ff1493',
+              borderRadius: 5,
+              marginLeft: 10,
+              marginRight: 10,
+              marginBottom: 10,
+              height: 210,
+            },
+            dateText: {
+              fontSize: 15,
+              color: '#191970',
+              paddingLeft: 20,
+            },
+            dateTouchBody: {
+              marginTop: 5,
+              marginBottom: 5,
+            },
+            datePicker: {
+              backgroundColor: '#9acd32',
             },
           }}
         />
@@ -63,7 +138,7 @@ class RandomDate extends Component {
           onPress={() =>
             this.state.isgoAhead ? navigate('time') : this.onDismiss()
           }>
-          <Text style={buttonTextStyle}>GoWether</Text>
+          <Text style={buttonTextStyle}>SUBMIT</Text>
         </TouchableOpacity>
       </View>
     );
@@ -86,14 +161,15 @@ const Container = StyleSheet.create({
     flex: 1,
   },
   buttonStyle: {
-    width: 138,
-    height: 35,
-    backgroundColor: '#00bfff',
+    width: 170,
+    height: 45,
+    backgroundColor: '#ff4500',
     marginTop: 20,
     borderRadius: 5,
   },
   buttonTextStyle: {
     alignSelf: 'center',
+    paddingTop: 10,
     paddingTop: 5,
     fontSize: 18,
   },
